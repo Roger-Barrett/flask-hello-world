9eced2eec9566010409baacd6ee4db2bd977c6a9
Roger Barrett <roger.barrettjr@gmail.com>
2025-11-11 06:32:52 +0000

    DB Insert, Select and Drop Added

--- app.py
+++ app.py
@@ -29,4 +29,53 @@ def creating():
     conn.close()
     return "Basketball Table Successfully Created" 
         
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://post_gres_test_db_user:mBZJ7jeiM34hnqMrIj44LCnvux1Zolwh@dpg-d49chr9r0fns738gbem0-a/post_gres_test_db")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0), 
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15), 
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2),
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
+
+@app.route('db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://post_gres_test_db_user:mBZJ7jeiM34hnqMrIj44LCnvux1Zolwh@dpg-d49chr9r0fns738gbem0-a/post_gres_test_db")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    contents = cur.fetchall()
+    conn.close()
+    response=""
+    response=response+"<table>"
+    for player in contents: 
+        response = response +"<tr"
+        for info in player:
+            response = response + "<td>{}</td>".format(info)
+        response = response + "</tr>"
+    response = response+"</tasble>"
+    return response
+
+@app.route('/db_drop')
+def dropping():
+    conn = psycopg2.connect("postgresql://post_gres_test_db_user:mBZJ7jeiM34hnqMrIj44LCnvux1Zolwh@dpg-d49chr9r0fns738gbem0-a/post_gres_test_db")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
+
+
+        
+    
     
